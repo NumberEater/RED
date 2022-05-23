dc622d6218c2b99d912e5ff076d748c8356ca996
NumberEater <reddickgrant@gmail.com>
2022-05-23 12:31:16 -0400

    Made slight writing optimizations

--- Writer.py
+++ Writer.py
@@ -4,13 +4,14 @@ import RGBConversions
 import pickle
 
 # If you want to questionably round the image, set this to True
-ROUND_VALUES = False
+ROUND_VALUES = True
 # If you want to questionably compress the image, set this to True
 COMPRESS_VALUES = False
 
 
-def RGBRound(x, base=40):
-    return base * round(x / base)
+# Round values to the intensity
+def RGBRound(x, intensity=20):
+    return intensity * round(x / intensity)
 
 
 def copy_image(image_path, name):
@@ -21,7 +22,7 @@ def copy_image(image_path, name):
 
     # Convert RGB to hex
     for i in range(len(values[1])):
-        if ROUND_VALUES:
+        if ROUND_VALUES:  # Rounding function
             if values[1][i][0] > 200:
                 if values[1][i][1] > 200:
                     if values[1][i][2] > 200:
@@ -30,7 +31,7 @@ def copy_image(image_path, name):
                 if values[1][i][1] < 55:
                     if values[1][i][2] < 55:
                         values[1][i] = (0, 0, 0)
-        elif COMPRESS_VALUES:
+        elif COMPRESS_VALUES:  # Compressing function
             values[1][i] = (RGBRound(values[1][i][0]), RGBRound(values[1][i][1]), RGBRound(values[1][i][2]))
         hex_value = RGBConversions.rgb_to_hex(values[1][i])
         split_hex_value = hex_value[0].split()
@@ -40,4 +41,7 @@ def copy_image(image_path, name):
     with open(name, "wb") as f:
         json_string = json.dumps(values)
         pickle.dump(json_string, f)
+        f.close()
+    with open("json_raw.json", "w") as f:
+        json.dump(values, f)
         f.close()
