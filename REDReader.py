09a0ba257466e52d61e4366792e3be76f287f65d
NumberEater <reddickgrant@gmail.com>
2022-05-23 13:22:11 -0400

    Made changes

--- REDReader.py
+++ REDReader.py
@@ -3,44 +3,43 @@ import json
 import pickle
 import RGBConversions
 
-image_path = input("RED FILE: ")
 
-with open(image_path, "rb") as f:
-    load = pickle.load(f)
-    load = json.loads(load)
-    f.close()
+def show_image(image_path):
+    with open(image_path, "rb") as f:
+        load = pickle.load(f)
+        load = json.loads(load)
+        f.close()
 
+    SCREEN_SIZE = WIDTH, HEIGHT = (load[0][0], load[0][1])
 
-SCREEN_SIZE = WIDTH, HEIGHT = (load[0][0], load[0][1])
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption('RED Reader')
+    fps = pygame.time.Clock()
+    paused = False
 
-pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption('RED Reader')
-fps = pygame.time.Clock()
-paused = False
+    x = 0
+    y = 0
 
-x = 0
-y = 0
+    screen.fill((175, 175, 175))
 
-screen.fill((175, 175, 175))
+    for i in range(len(load[1])):
+        if len(load[1][i]) == 1:
+            load[1][i] = load[1][i] + load[1][i] + load[1][i] + load[1][i] + load[1][i] + load[1][i]
+        try:
+            screen.set_at((x, y), RGBConversions.hex_to_rgb(load[1][i]))
+        except IndexError:
+            print("something went wrong IndexError")
+            exit(-1)
+        if x == load[0][0] - 1:
+            x = 0
+            y += 1
+        else:
+            x += 1
 
-for i in range(len(load[1])):
-    if len(load[1][i]) == 1:
-        load[1][i] = load[1][i] + load[1][i] + load[1][i] + load[1][i] + load[1][i] + load[1][i]
-    try:
-        screen.set_at((x, y), RGBConversions.hex_to_rgb(load[1][i]))
-    except IndexError:
-        print("something went wrong IndexError")
-        exit(-1)
-    if x == load[0][0] - 1:
-        x = 0
-        y += 1
-    else:
-        x += 1
+    pygame.display.update()
 
-pygame.display.update()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
