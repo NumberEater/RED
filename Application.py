ce00d18b5acb09f85bea7e549d515e0ad82f5fad
NumberEater <reddickgrant@gmail.com>
2022-05-23 13:22:02 -0400

    Added additional options

--- Application.py
+++ Application.py
@@ -1,13 +1,13 @@
 import Reader
 import Writer
-from PIL import Image
+import REDReader
 
 
 def main():
     print("RED file format\n")
-    print("1) Convert image")
-    print("2) Read image")
-    print("3) Convert and Read")
+    print("1) Convert to RED")
+    print("2) Convert to PNG")
+    print("3) Convert and Show")
     choice = input(">>> ")
 
     if choice == "1":
@@ -20,8 +20,7 @@ def main():
     else:
         image_path = input("Image: ")
         Writer.copy_image(image_path, "output.red")
-        Reader.read("output.red")
-        Image.open("output.red.png").show()
+        REDReader.show_image("output.red")
 
 
 main()
