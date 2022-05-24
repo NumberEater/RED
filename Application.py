a236778b6ffdcb7100e8df6f8c6afa9a0f64513b
NumberEater <reddickgrant@gmail.com>
2022-05-24 09:00:59 -0400

    Added REDReader option

--- Application.py
+++ Application.py
@@ -7,7 +7,8 @@ def main():
     print("RED file format\n")
     print("1) Convert to RED")
     print("2) Convert to PNG")
-    print("3) Convert and Show")
+    print("3) Show in RED Reader")
+    print("4) Convert and Show")
     choice = input(">>> ")
 
     if choice == "1":
@@ -15,8 +16,11 @@ def main():
         image_output_name = input("Output name: ")
         Writer.copy_image(image_path, image_output_name)
     elif choice == "2":
-        image_path = input("Image to read: ")
-        Reader.read(image_path)
+        image_path = input("Image to convert: ")
+        Reader.convert(image_path)
+    elif choice == "3":
+        image_path = input("Image to show: ")
+        REDReader.show_image(image_path)
     else:
         image_path = input("Image: ")
         Writer.copy_image(image_path, "output.red")
