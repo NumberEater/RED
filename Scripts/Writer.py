36e697493e4d877b01d54649d2394a70d2ba911a
NumberEater <reddickgrant@gmail.com>
2022-05-26 21:10:08 -0400

    Added logger

--- Scripts/Writer.py
+++ Scripts/Writer.py
@@ -1,43 +1,22 @@
 from PIL import Image
 import json
-from Scripts import RGBConversions
+from Scripts import RGBConversions, Logging
 import pickle
 
-# If you want to questionably round the image, set this to True
-ROUND_VALUES = False
-# If you want to questionably compress the image, set this to True
-COMPRESS_VALUES = False
 
-
-# Round values to the intensity
-def RGBRound(x, intensity=20):
-    return intensity * round(x / intensity)
-
-
-def copy_image(image_path, name):
+def copy_image(image_path):
+    writer_logger = Logging.Logger(Logging.Mode.MODE_ERROR)
     image = Image.open(image_path, "r")
     values = list(image.getdata())
 
     values = [image.size, values]
 
     for i in range(len(values[1])):
-        if ROUND_VALUES:  # Rounding function
-            if values[1][i][0] > 200:
-                if values[1][i][1] > 200:
-                    if values[1][i][2] > 200:
-                        values[1][i] = (255, 255, 255)
-            elif values[1][i][0] < 55:
-                if values[1][i][1] < 55:
-                    if values[1][i][2] < 55:
-                        values[1][i] = (0, 0, 0)
-        elif COMPRESS_VALUES:  # Compressing function
-            values[1][i] = (RGBRound(values[1][i][0]), RGBRound(values[1][i][1]), RGBRound(values[1][i][2]))
         hex_value = RGBConversions.rgb_to_hex(values[1][i])
-        split_hex_value = hex_value[0].split()
-
         values[1][i] = hex_value
 
-    with open(name, "wb") as f:
+    with open("output.red", "wb") as f:
         json_string = json.dumps(values)
         pickle.dump(json_string, f)
+        writer_logger.Info("Saved as output.red")
         f.close()
